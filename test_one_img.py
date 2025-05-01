8be14ed87bd8840426bb6ae6f5b7a569e476149d
Laura Mas <lauramas@mail.fresnostate.edu>
2025-04-30 21:45:10 -0700

    Fixed name

--- test_one_img.py
+++ test_one_img.py
@@ -2,9 +2,11 @@ from src.dataset import load_image
 from src.preprocessing import binarize_image, thin_image
 from src.minutiae_extraction import extract_minutiae_CN, extract_minutiae_Harris
 from src.display import plot_minutiae
+import os
 
 # Choose one fingerprint image from SOCOFing
-img_path = "data/SOCOFing/Real/101__M__Left__index__finger.BMP"  # adjust if needed
+img_path = "data/SOCOFing/Real/101__M_Left_index_finger.BMP"  # adjust if needed
+print(os.path.exists(img_path))  # Should print True
 img = load_image(img_path)
 
 # --- CN Method ---
