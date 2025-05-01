320fb35c9d227d064b151825021e79f4907ed976
Laura Mas <lauramas@mail.fresnostate.edu>
2025-04-30 21:01:51 -0700

    Feature vector implementation

---
+++ src/features.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def extract_feature_vector(minutiae, img_shape, grid_size=8):
+    """Generate grid histogram feature vector from minutiae points."""
+    h, w = img_shape
+    grid = np.zeros((grid_size, grid_size))
+
+    for (y, x) in minutiae:
+        row = min(grid_size-1, int(y / (h / grid_size)))
+        col = min(grid_size-1, int(x / (w / grid_size)))
+        grid[row, col] += 1
+
+    return grid.flatten()
